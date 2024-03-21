1289a6def1a57a263b934ec07630b03df23851f3
jiangzhen-cloud <zhen.23@intl.zju.edu.cn>
2024-03-21 20:39:28 +0800

    Practical4

---
+++ Practical4/culture_density.py
@@ -0,0 +1,13 @@
+#a1=0.05
+#while an<0.9:
+#an=2an-1
+#if an>0.9:
+#DONE  print(n)
+
+
+a=0.05
+n=0
+while a<0.9:
+    a=2*a
+    n+=1
+print("I can stay away from the lab "+str(n)+" days.")
---
+++ Practical4/recursive.py
@@ -0,0 +1,5 @@
+a=1                            #a0=1,a1=4
+for i in range(1,6):           #a2=2a1+3=11
+    a=2*a+3                    #repeat for 5 times
+    print(a)                   #print a1,a2,a3,a4,a5
+                            
