fd79d2d0574c0f9b1960f91bdb62951e80856447
jiangzhen-cloud <zhen.23@intl.zju.edu.cn>
2024-05-19 21:59:39 +0800

    commit

--- Practical6/average_day.py
+++ Practical6/average_day.py
@@ -1,7 +1,8 @@
-dic={"Sleeping":8,"Classes":6,"Studying":3.5,"TV":2,"Music":1,"other":3.5}           #Create a dictionary
+Sleep=8                                                                              #The requested activity that can be modified.
+dic={"Sleeping":Sleep,"Classes":6,"Studying":3.5,"TV":2,"Music":1,"other":11.5-Sleep}           #Create a dictionary
 import matplotlib.pyplot as plt
 class_labels=["Sleeping","Classes","Studying","TV","Music","other"]                  #Set up the labels
-time_week=[8,6,3.5,2,1,3.5]                                                          #Set the data
+time_week=[Sleep,6,3.5,2,1,11.5-Sleep]                                                          #Set the data
 Studying_and_esle=[0,0,0.1,0,0,0]                                                    #Set the highlighted sections
 plt.figure()
 plt.pie(time_week,labels=class_labels,startangle=90,explode=Studying_and_esle)       #Draw the pie chart
